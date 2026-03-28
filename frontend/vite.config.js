10d441b7c0b4cfdaa3db59e05931b132c4d349fa
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2026-03-28 15:12:39 +0530

    backend_Link_added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://act-assignment-wg3e.onrender.com',
         changeOrigin: true,
       },
     },
